6a6b0b00d2312737b5a2932e79b849a3964e8659
Aaron Roberson <aaronaroberson@gmail.com>
2015-10-28 20:23:21 -0700

    chore(package): set fourseven:scss dependency to 3.2.0
    Roll fourseven:scss back to 3.2.0 or greater to account for breaking changes in 3.3.x and 3.4.x. Also bumped version to 3.3.5_3.
    
    Closes #1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'dyaa:bootstrap-sass-only',
-	version: '3.3.5_2',
+	version: '3.3.5_3',
 	// Brief, one-line summary of the package.
 	summary: 'Bootstrap with Sass Only',
 	// URL to the Git repository containing the source code for this package.
@@ -13,7 +13,8 @@ Package.onUse(function (api) {
 	api.versionsFrom('METEOR@1.2');
 
 	api.use([
-		'meteor'
+		'meteor',
+		'fourseven:scss@3.2.0',
 	]);
 
 	api.addAssets([
